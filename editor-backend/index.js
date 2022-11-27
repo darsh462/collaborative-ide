57ff731cc9a9f4605b34bc050cd916c806dfb1e1
ayush1729cpp <75876302+ayush1729cpp@users.noreply.github.com>
2022-11-27 14:31:57 +0530

    Update index.js

--- editor-backend/index.js
+++ editor-backend/index.js
@@ -29,7 +29,7 @@ app.use(
 );
 app.get("*", (req, res) => {
     res.sendFile(
-        path.join(__dirname, "../client/build/index.html")
+        path.join(__dirname, "../editor/build/index.html")
     );
 })
 
